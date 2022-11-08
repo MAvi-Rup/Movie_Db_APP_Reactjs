f584e92be83bcc0d49228a6b4aaa499f7f4a9cca
MAvi-Rup <avirupmondal19@gmail.comm>
2022-11-08 17:32:36 +0600

    Firebase Added

---
+++ src/firebase.init.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyABMjqyRNGgmwBTd_RGZltm_32x8EMXfi4",
+  authDomain: "movie-streaming-website-18.firebaseapp.com",
+  projectId: "movie-streaming-website-18",
+  storageBucket: "movie-streaming-website-18.appspot.com",
+  messagingSenderId: "213865262478",
+  appId: "1:213865262478:web:9bc645d090f4efe37008df"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
